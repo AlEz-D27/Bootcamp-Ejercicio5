c203229059a820ad84e434dcba6a3ec7e3bc9c2a
Duarte <alfre27ezequiel@gmail.com>
2023-05-02 13:35:48 -0400

    Quinto ejercicio de BootCamp

---
+++ Ejercicio5.js
@@ -0,0 +1,20 @@
+// Mi altura en centimetros
+let altura_mts = 178;
+
+// Mi altura en metros
+let altura_cm = 1.78;
+
+// Mi peso en kilogramos
+let peso_kg = 72.7;
+
+// Mi altura en metros redondeado hacia arriba
+let altura = Math.ceil(altura_cm);
+console.log(altura);
+
+// Mi peso en kilogramos redondeado hacia abajo
+let peso = Math.floor(peso_kg);
+console.log(peso);
+
+// Valor maximo + 1
+let max_valor_Js = ( Number.MAX_VALUE + 1 ) === Number.MAX_VALUE;
+console.log(max_valor_Js);
